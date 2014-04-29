a1e4d0c3dfd910b833b43439390bb2887a66ef19
Shocuul <shocuul@live.com>
2014-04-29 17:03:17 -0500

    Chapter 13

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,7 @@ class Product < ActiveRecord::Base
 		Product.order(:updated_at).last
 	end
 	has_many :line_items
+	has_many :orders, through: :line_items
 
 	before_destroy :ensure_not_referenced_by_any_line_item
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -38,6 +42,7 @@ Depot::Application.routes.draw do
   #     resources :comments, :sales
   #     resource :seller
   #   end
+
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
